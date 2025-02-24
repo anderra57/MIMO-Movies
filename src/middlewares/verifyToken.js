39bdb1e70c4d75bec3599dddc28be68aea5d66c8
Ander Prieto <aprietolo@upsa.es>
2025-02-24 20:30:54 +0100

    Instalados módulos de autenticación e implementados middlewares

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,24 @@
+const { decodeAccessToken } = require("../auth");
+
+module.exports = {
+  verifyToken: (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "Acceso no autorizado" });
+    }
+
+    const token = authHeader.split(" ")[1] ?? "";
+
+    const decoded = decodeAccessToken(token);
+
+    if (decoded === null) {
+      return res.status(401).json({ message: "Acceso no autorizado" });
+    }
+
+    req.userId = decoded.id;
+    console.log("userId: ", req.userId);
+
+    next();
+  },
+};
